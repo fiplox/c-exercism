579fbe247bc502179e779ee3209d5b04467766cf
fiplox <56274824+fiplox@users.noreply.github.com>
2021-08-07 22:05:04 +0200

    1st iteration

---
+++ gigasecond/src/gigasecond.c
@@ -0,0 +1,6 @@
+#include "gigasecond.h"
+
+time_t gigasecond_after(time_t moment)
+{
+	return moment + GIGASECOND;
+}
---
+++ gigasecond/src/gigasecond.h
@@ -0,0 +1,10 @@
+#ifndef GIGASECOND_H
+#define GIGASECOND_H
+
+#include <time.h>
+
+#define GIGASECOND 1000000000
+
+time_t gigasecond_after(time_t);
+
+#endif
