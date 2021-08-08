9b6384807b90b95a7decbf1e86e0de17fb9da892
fiplox <56274824+fiplox@users.noreply.github.com>
2021-08-08 15:32:52 +0200

    2nd iteration

--- luhn/src/luhn.c
+++ luhn/src/luhn.c
@@ -1,43 +1,21 @@
 #include "luhn.h"
 
-#include <string.h>
 #include <ctype.h>
+#include <string.h>
 
 bool luhn(const char *num)
 {
-	size_t i, j;
-	size_t len = strlen(num);
-	int sum = 0, digit;
-	char num_normalized[64];
-
-	for (i = 0, j = 0; i < len; ++i) {
+	int len = strlen(num);
+	int sum = 0, n = 0;
+	int digit;
+	for (int i = len - 1; i >= 0; --i) {
 		if (num[i] == ' ')
 			continue;
 		if (!isdigit(num[i]))
 			return false;
-
-		num_normalized[j++] = num[i];
+		digit = num[i] - '0';
+		n++;
+		sum += n % 2 ? digit : digit < 9 ? (digit + digit) % 9 : 9;
 	}
-	num_normalized[j] = '\0';
-	len = strlen(num_normalized);
-	
-	if (len <= 1)
-		return false;
-
-	bool doubling = strlen(num_normalized) % 2 == 0;
-	
-	for (i = 0; i < len; ++i) {
-		digit = num_normalized[i] - '0';
-		if (digit < 0 || digit > 9)
-			return false;
-		if (doubling) {
-			digit *= 2;
-			if (digit > 9)
-				sum -= 9;
-		}
-		sum += digit;
-		doubling = !doubling;
-	}
-
-	return sum % 10 == 0;
+	return n > 1 && sum % 10 == 0;
 }
