20b6789b9a106eed63e8a8b01c3a13c89dab001f
fiplox <56274824+fiplox@users.noreply.github.com>
2021-08-08 13:18:37 +0200

    2nd iteration

--- leap/src/leap.c
+++ leap/src/leap.c
@@ -2,7 +2,5 @@
 
 bool leap_year(int year)
 {
-	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-		return true;
-	return false;
+	return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
 }
